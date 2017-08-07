cca303d43fa1a76a7effecd40c578dd4a37f400f
PranathiB <pranathi.birudugadda@goeuro.com>
2017-08-07 15:31:24 +0200

    DUmmy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var handlebar = require('express-handlebars');
 
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
 var routes = require('./routes/index');
 
 var app = express();
@@ -13,6 +15,7 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 router.get("/",function(req,res){
     res.render("index");
 
 });
+
+router.verifyPassPhrase = function (req, res) {
+    req.checkBody('password', 'Password is required').notEmpty();
+    var pass = req.body.password;
+    if(pass==="hello"){
+        res.render("success");
+    }
+    else{
+        res.render("error")
+    }
+};
+router.post("/", router.verifyPassPhrase);
+
+
+router.yesOrNo = function (req, res) {
+    req.checkBody('YesOrNo', 'Enter your answer').notEmpty();
+    var answer1 = req.body.YesOrNo;
+    if(answer1==="yes" || answer1 === "ya"){
+        res.render("quizSteps1");
+    }
+    else{
+        res.render("success");
+    }
+
+};
+router.post("/quiz-steps", router.yesOrNo);
+router.get("/quiz-steps", function (req, res) {
+    res.render("quiz-steps")
+});
+
+router.meetLocation = function (req, res) {
+    req.checkBody('answer', 'Enter your answer').notEmpty();
+    var ans = req.body.answer;
+    if(ans==="a"){
+        res.render("quizSteps2");
+    }
+    else{
+        res.render("quizSteps1");
+    }
+};
+router.post("/quizSteps1", router.meetLocation);
+router.get("/quizSteps1", function (req, res) {
+    res.render("quizSteps1")
+});
+
 
 module.exports = router;
